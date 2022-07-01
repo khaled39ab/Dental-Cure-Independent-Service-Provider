25e6ecb00a80063aade8ee0163542045ef0e2312
khaled39ab <khaled39ab@gmail.com>
2022-07-02 00:38:24 +0600

    home page complete

--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -12,21 +12,21 @@ const Features = () => {
                     <img src={features1} className="card-img-top py-3" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title fs-4 mb-3">Easy Appointment</h5>
-                        <p className="card-text pb-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text pb-3">With the simple steps, appointments can be booked online with by checking doctors' schedules.</p>
                     </div>
                 </div>
                 <div className="card p-3 card-container" style={{ width: '18rem' }}>
                     <img src={features2} className="card-img-top py-3" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title fs-4 mb-3">Emergency Service</h5>
-                        <p className="card-text pb-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text pb-3">Call me when you have fractured teeth, severe toothaches, severe gum injuries, gum disease, something other than food lodged.</p>
                     </div>
                 </div>
                 <div className="card p-3 card-container" style={{ width: '18rem' }}>
                     <img src={features3} className="card-img-top py-3" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title fs-4 mb-3">24/7 Service</h5>
-                        <p className="card-text pb-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text pb-3">24/7 patient support. Patient can get help and find answers to questions as soon as they come up and in real-time.</p>
                     </div>
                 </div>
             </div>
