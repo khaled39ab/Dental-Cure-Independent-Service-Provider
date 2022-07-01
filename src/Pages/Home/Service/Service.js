ba066cb4121701705a4974ad8f1e5bbc048f3adf
khaled39ab <khaled39ab@gmail.com>
2022-07-01 22:51:18 +0600

    services section complete and make responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Button, Card, CardGroup } from 'react-bootstrap';
+import './Service.css'
 
-const Service = ({service}) => {
+const Service = ({ service }) => {
+    const { name, img, price, about } = service;
     return (
         <div>
-            <h2>This is service</h2>
+            <div className='service-container'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Subtitle className='my-2'>{price}</Card.Subtitle>
+                        <Card.Text className='service-about'>
+                            {about}
+                        </Card.Text>
+                        <Button variant="primary">Appointment</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
         fetch('services.json')
-        .then(res => res.json())
-        .then( data => setServices(data))
-        .catch(err => console.log(err))
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data))
+            .catch(err => console.log(err))
+    }, [])
 
     return (
-        <div>
-            {
-                services.map (service =><Service
-                    key={service.id}
-                    service = {service}
-                ></Service>)
-            }
+        <div id='services' className='services-section'>
+            <div className='container'>
+                <h1 className='py-4 text-center services-title'>Our Services</h1>
+                <div className='services-container'>
+                    {
+                        services.map(service => <Service
+                            key={service.id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
