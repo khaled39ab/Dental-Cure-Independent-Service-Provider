9dec2c34e93b1dc34cc65e57184aabf8e3e90653
khaled39ab <khaled39ab@gmail.com>
2022-07-03 00:25:07 +0600

    make a protected page named appointment

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Services from './Pages/Home/Services/Services';
 import LogIn from './Pages/LogIn/LogIn';
 import NotFound from './Pages/NotFound/NotFound';
 import Register from './Pages/Register/Register';
+import Appointment from './Pages/Shared/Appointment/Appointment';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import RequireAuth from './Pages/Shared/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -22,6 +24,11 @@ function App() {
         <Route path='/aboutMe' element={<AboutMe></AboutMe>}></Route>
         <Route path='/login' element={<LogIn></LogIn>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/appointment' element={
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>
+        }></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
