2f6605bf01294807d74f49df2db5275496a3da3c
khaled39ab <khaled39ab@gmail.com>
2022-07-02 23:00:45 +0600

    login component done

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './LogIn.css'
 
 const LogIn = () => {
     return (
         <div>
-            <h2>Login</h2>
-        </div>
+            <div className='w-50 mx-auto px-3 py-4  my-3 border border-4 rounded-3'>
+            <h1 className='text-success text-center'>Please Log In</h1>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Log in
+                    </Button>
+                    <Button variant="link">Forget Password?</Button>
+                    <hr />
+                </Form>
+                <div className='create-acc-btn'>
+                    <Link to={'/register'}>
+                        <Button>
+                            Create An Account
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </div >
     );
 };
 
