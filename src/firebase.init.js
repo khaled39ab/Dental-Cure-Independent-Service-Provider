959bcb802b7a9ba28042cd64fcd6252626de4d64
khaled39ab <khaled39ab@gmail.com>
2022-07-04 22:54:49 +0600

    make secure the firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRIUe2U17A7swBj8dZxRhug31FYvZ0QSw",
-  authDomain: "independent-service-prov-296b0.firebaseapp.com",
-  projectId: "independent-service-prov-296b0",
-  storageBucket: "independent-service-prov-296b0.appspot.com",
-  messagingSenderId: "712571992428",
-  appId: "1:712571992428:web:e1677cdd413fc77e1f5b49"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
