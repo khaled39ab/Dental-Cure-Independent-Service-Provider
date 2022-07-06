71fbae032f665c3302e05365c3f59faa2c4360f8
khaled39ab <khaled39ab@gmail.com>
2022-07-06 22:44:35 +0600

    write Benefit of Firebase

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBox, faDatabase, faCloudArrowDown, faCloudArrowUp, faLink, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+
 
 const Blogs = () => {
     return (
         <div className='container mt-4'>
-            <div className='my-5'>
-                <h2> <span className='text-warning'>QUESTION:</span>  Difference between authorization and authentication?</h2>
+            <div className='my-5 pb-3'>
+                <h2> <span className='text-warning'>QUESTION:</span>  Difference between authentication and authorization?</h2>
                 <h4 className='text-info'>Authentication vs. Authorization</h4>
 
                 <Table striped>
@@ -46,12 +50,55 @@ const Blogs = () => {
                 </Table>
             </div>
 
-            <div className='my-5 pt-5'>
+            <div className='my-5 py-5'>
                 <h2> <span className='text-warning'>QUESTION:</span> Why are you using firebase? What other options do you have to implement authentication?</h2>
+                <h4 className='text-info mt-4 mb-3'>Benefit of Firebase</h4>
+                <ListGroup>
+                    <ListGroup.Item variant="primary" className='d-flex'>
+                        <div className='me-3' style={{ fontSize: '2rem' }}>
+                            <FontAwesomeIcon icon={faDatabase} />
+                        </div>
+                        <div>
+                            <p>Firebase Authentication gives back-end development services, simple-to-use SDKs, and instant UI libraries to confirm clients over application. Firebase takes care of the secure logins. It keeps a track and identifies the user who logged in and who logged out from the app/web. Without Firebase, it would have been difficult to implement the process securely and seamlessly.</p>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="secondary" className='d-flex'>
+                        <div className='me-3' style={{ fontSize: '2rem' }}>
+                            <FontAwesomeIcon icon={faCloudArrowDown} />
+                        </div>
+                        <div>
+                            <p> A real-time Database is a cloud-hosted database. Firebase Realtime database allows applications to approach cross-platform data within realtime after joining NoSQL cloud-storage. Firebase was originally developed to be a realtime database, and today that is still one of its key features.</p>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="success" className='d-flex'>
+                        <div className='me-3' style={{ fontSize: '2rem' }}>
+                            <FontAwesomeIcon icon={faCloudArrowUp} />
+                        </div>
+                        <div>
+                            <p>Clouds storage is built for application developers who need to store and serve user-generated content, for example, photos or videos. This cloud database is also known for its Realtime updates, flexible data models, offline support, and quick data inquiries.</p>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="danger" className='d-flex'>
+                        <div className='me-3' style={{ fontSize: '2rem' }}>
+                            <FontAwesomeIcon icon={faLink} />
+                        </div>
+                        <div>
+                            <p>Firebase Dynamic Links are smart URLs that enable marketers to promote the app across various external channels. This feature launches a dynamic link of your app for customized user experience.</p>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="warning" className='d-flex'>
+                        <div className='me-3' style={{ fontSize: '2rem' }}>
+                            <FontAwesomeIcon icon={faLaptopCode} />
+                        </div>
+                        <div>
+                            <p>Provides A Free Start to Newbies a newbie programmer and willing to know the core advantage of Firebase, then you consider its 'free start' feature.</p>
+                        </div>
+                    </ListGroup.Item>
+                </ListGroup>
             </div>
 
             <div className='my-5 pt-5'>
-                <h2> <span className='text-warning'>QUESTION:</span> What other services does firebase provide other than authentication</h2>
+                <h2> <span className='text-warning'>QUESTION:</span> What other services does firebase provide other than authentication?</h2>
             </div>
         </div>
     );
